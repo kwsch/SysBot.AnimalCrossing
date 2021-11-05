9730a72d3ecc5a01fdd5de20287389d087c3edd2
hp3721 <44118666+hp3721@users.noreply.github.com>
2021-11-04 20:46:38 -0500

    Update Offsets.cs

--- CrossBot.Core/Offsets.cs
+++ CrossBot.Core/Offsets.cs
@@ -1,4 +1,4 @@
-﻿using NHSE.Core;
+using NHSE.Core;
 
 namespace CrossBot.Core
 {
@@ -8,31 +8,25 @@ namespace CrossBot.Core
     public static class Offsets
     {
         // Helpers
-        public const uint PlayerSize = 0x10E500;
+        public const uint PlayerSize = 0x11B968;
 
-        public const uint InventoryOffset = 0xAED22840; // player 0 (A)
-        public const string CoordinatePointer = "[[[[main+3A33980]+18]+178]+D0]+DA";
+        public const uint InventoryOffset = 0xAF70D6E0; // player 0 (A)
+        public const string CoordinatePointer = "[[[[main+460ED68]+18]+178]+D0]+DA";
 
         // Main player offsets functions
         public static uint GetPlayerInventoryOffset(uint profiles) => (InventoryOffset + (PlayerSize * (profiles - 1)));
 
         // Main save offsets
-        public const uint TurnipAddress = 0xADD1BB84;
-        public const uint VillagerAddress = TurnipAddress - 0x2d40 - 0x43be2c + 0x10;
-        public const uint VillagerHouseAddress = TurnipAddress - 0x2d40 - 0x43be2c + 0x43abe4;
+        public const uint TurnipAddress = 0xAE6030F4;
+        public const uint VillagerAddress = TurnipAddress - 0x2D40 - 0x45B50C + 0x10;
+        public const uint VillagerHouseAddress = TurnipAddress - 0x2D40 - 0x45B50C + 0x44f7FC;
 
         public static uint GetVillagerOffset(int index) => VillagerAddress + (uint)(Villager2.SIZE * index);
         public static uint GetVillagerHouseOffset(int index) => VillagerHouseAddress + (uint)(Villager2.SIZE * index);
 
-        public const uint FieldItemStart = VillagerAddress - 0x10 + 0x22e1b8;
+        public const uint FieldItemStart = VillagerAddress - 0x10 + 0x22F3F0;
 
-        // Other Addresses
-     // public const uint ArriverNameLocAddress = 0xB66D6FF8;
-     // public const ulong ArriverVillageLocAddress = ArriverNameLocAddress - 0x1C;
-     // public const uint TextSpeedAddress = 0xBACF20C;
-        public const uint DodoAddress = 0xA99015C;
-        public const uint OnlineSessionAddress = 0x920F740;
-     // public const ulong OnlineSessionVisitorAddress = 0x9D3EFB0;
-     // public const ulong OnlineSessionVisitorSize = 0x448; // reverse order
+        public const uint DodoAddress = 0xABCF15C;
+        public const uint OnlineSessionAddress = 0x944E740;
     }
 }
