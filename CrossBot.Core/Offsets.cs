f73894a7ea0a63545296730dd5f99d07e9faf896
hp3721 <44118666+hp3721@users.noreply.github.com>
2022-02-16 09:52:02 -0600

    Update offsets for 2.0.5
    Thanks to @berichan for the offsets.

--- CrossBot.Core/Offsets.cs
+++ CrossBot.Core/Offsets.cs
@@ -10,14 +10,14 @@ namespace CrossBot.Core
         // Helpers
         public const uint PlayerSize = 0x11B968;
 
-        public const uint InventoryOffset = 0xAF71E6E0; // player 0 (A)
+        public const uint InventoryOffset = 0xAFB1E6E0; // player 0 (A)
         public const string CoordinatePointer = "[[[[main+4627088]+18]+178]+D0]+DA";
 
         // Main player offsets functions
         public static uint GetPlayerInventoryOffset(uint profiles) => (InventoryOffset + (PlayerSize * (profiles - 1)));
 
         // Main save offsets
-        public const uint TurnipAddress = 0xAE6140F4;
+        public const uint TurnipAddress = 0xAEA140F4;
         public const uint VillagerAddress = TurnipAddress - 0x2D40 - 0x45B50C + 0x10;
         public const uint VillagerHouseAddress = TurnipAddress - 0x2D40 - 0x45B50C + 0x44f7FC;
 
