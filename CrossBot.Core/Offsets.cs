4957f94bd8a7588669da6c4a83d26a3848640eae
DESKTOP-SN9KF5O\Strawberry <berichandev@gmail.com>
2026-01-19 15:53:28 +0000

    Update offsets for 3.0.0

--- CrossBot.Core/Offsets.cs
+++ CrossBot.Core/Offsets.cs
@@ -8,24 +8,24 @@ namespace CrossBot.Core;
 public static class Offsets
 {
     // Helpers
-    public const uint PlayerSize = 0x11B968;
+    public const uint PlayerSize = 0x131F70;
 
-    public const uint InventoryOffset = 0xAFB1E6E0; // player 0 (A)
-    public const string CoordinatePointer = "[[[[main+4627088]+18]+178]+D0]+DA";
+    public const uint InventoryOffset = 0xB27BB758; // player 0 (A)
+    public const string CoordinatePointer = "[[[[main+4BF9E30]+18]+178]+D0]+DA";
 
     // Main player offsets functions
     public static uint GetPlayerInventoryOffset(uint profiles) => InventoryOffset + (PlayerSize * (profiles - 1));
 
     // Main save offsets
-    public const uint TurnipAddress = 0xAEA140F4;
-    public const uint VillagerAddress = TurnipAddress - 0x2D40 - 0x45B50C + 0x10;
-    public const uint VillagerHouseAddress = TurnipAddress - 0x2D40 - 0x45B50C + 0x44f7FC;
+    public const uint TurnipAddress = 0xB14DBB30;
+    public const uint VillagerAddress = TurnipAddress - 0x2d40 - 0x48d920 + 0x10;
+    public const uint VillagerHouseAddress = TurnipAddress - 0x2d40 - 0x48d920 + 0x481c10;
 
     public static uint GetVillagerOffset(int index) => VillagerAddress + (uint)(Villager2.SIZE * index);
     public static uint GetVillagerHouseOffset(int index) => VillagerHouseAddress + (uint)(VillagerHouse2.SIZE * index);
 
-    public const uint FieldItemStart = VillagerAddress - 0x10 + 0x22F3F0;
+    public const uint FieldItemStart = VillagerAddress - 0x10 + 0x22f3f0;
 
-    public const uint DodoAddress = 0xABE015C;
-    public const uint OnlineSessionAddress = 0x945F740;
+    public const uint DodoAddress = 0xAC1A164;
+    public const uint OnlineSessionAddress = 0x9499748;
 }
